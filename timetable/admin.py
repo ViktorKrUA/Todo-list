c4df0c9e9087a60f89c05d6a505aada8ab1ca6dd
ViktorKr <kravchukviktors@gmail.com>
2023-03-21 15:59:33 +0200

    Added admin to timetable

--- timetable/admin.py
+++ timetable/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from timetable.models import Tag, Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_filter = ("tags", "deadline", "created")
+
+
+admin.site.register(Tag)
